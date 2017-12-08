6b7cabece24dc349f54490033764e3afdd36d5ce
Teranis 2k17 <teranis2k17@gmail.com>
2017-12-08 12:17:44 +0530

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def welcome
+    render html: "Welcome to the Redpanthers : Employee Portal"
+  end
 end
